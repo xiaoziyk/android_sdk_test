feda5cdb72c9c02926389bc31242ee3393ab6b79
yankai <carl.wang@minerva-games.com>
2022-03-09 16:44:52 +0800

    create

---
+++ TestSDK/src/main/java/com/moon/testsdk/LogUtils.java
@@ -0,0 +1,12 @@
+package com.moon.testsdk;
+
+import android.util.Log;
+
+/**
+ * Author by carl, Date on 2022/3/9.
+ */
+public class LogUtils {
+    public void showLog(String tag, String msg) {
+        Log.e(tag, msg);
+    }
+}
